2c77393fec28cf7a1bff24a05b0142101b1d1d3d
Yunseong Ha <gci28267@gmail.com>
2020-10-13 09:28:27 +0900

    prefix 생성기 업로드 등
    1. prefix 생성기 업로드
    2. 파일 정리

--- tools/prefix.py
+++ tools/prefix.py
@@ -1,24 +1,28 @@
 import os
-from tools import parsing
+
+ROOT_DIR    = '.\\'
+KOREAN_PRE  = 0
+ENGLISH_PRE = 1
+
+IN_DIR  = ROOT_DIR+'in\\'
+OUT_DIR = ROOT_DIR+'out\\'
 
 
 if __name__=='__main__':
-    files=os.listdir()
+    names=os.listdir(IN_DIR)
+    
+    if not os.path.isdir(OUT_DIR):
+        os.mkdir(OUT_DIR)
 
-    for file in files:
-        if os.path.isfile(file) and os.path.splitext(file)[1]=='.csv':
-            with open(file,'r',encoding='utf-8') as fr:
-                lines=fr.readlines()
+    for name in names:
+        if os.path.isfile(IN_DIR+name) and name.endswith('.csv'):
+            with open(IN_DIR+name,'r',encoding='utf-8') as file:
+                words=file.readlines()
             
-            res=[]
-            for line in lines:
-                if line.count('\t')>1:
-                    a,b=line.replace('\n','').split('\t')[1:3]
-                    if b.startswith('-'):
-                        b="'"+b
-                    res.append((a,b))
-            res.sort()
-            
-            with open(os.path.splitext(file)[0]+'.tsv','w',encoding='cp949') as fw:
-                for r in res:
-                    fw.write('\t'.join(r)+'\n')
+            with open(OUT_DIR+name,'w',encoding='utf-8') as out_file:
+                for k,word in enumerate(words):
+                    eng,kor=word.replace('\n','').split('\t')[:2]
+                    eng_pre=eng[:ENGLISH_PRE]
+                    kor_pre=kor[:KOREAN_PRE]
+                    out_file.write(('\t').join((eng,kor,eng_pre,kor_pre))+'\n')
+                out_file.flush()
